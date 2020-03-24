0929b6c603924227553860726cc92c8159a0ebc5
Daniel Grieco <danny26.business@gmail.com>
2020-03-24 19:07:44 +0100

    Initial Commit

---
+++ src/TextSnake.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : TextSnake.cpp
+// Author      : Daniel Grieco
+// Version     :
+// Copyright   : All Rights Reserved. Owned by Daniel Grieco ©
+// Description : Snake game in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
