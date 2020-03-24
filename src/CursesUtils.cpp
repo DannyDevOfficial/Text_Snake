40307198fd555de8bb352c2039908b26b2cd35a7
Daniel Grieco <danny26.business@gmail.com>
2020-03-24 20:12:56 +0100

    Created a header and source file for the Curses wrapper

---
+++ src/CursesUtils.cpp
@@ -0,0 +1,10 @@
+/*
+ * CursesUtils.cpp
+ *
+ *  Created on: Mar 24, 2020
+ *      Author: danielgrieco
+ */
+
+#include "CursesUtils.h"
+
+
---
+++ src/CursesUtils.h
@@ -0,0 +1,15 @@
+/*
+ * CursesUtils.h
+ *
+ *  Created on: Mar 24, 2020
+ *      Author: danielgrieco
+ */
+
+#ifndef CURSESUTILS_H_
+#define CURSESUTILS_H_
+
+
+
+
+
+#endif /* CURSESUTILS_H_ */
