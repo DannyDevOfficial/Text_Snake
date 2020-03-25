4a6dcc3d28bae5fa7b417546c6dc390162727124
Daniel Grieco <danny26.business@gmail.com>
2020-03-25 23:49:41 +0100

    Added functions to handle initialization and shutdown of the curses library

--- src/CursesUtils.cpp
+++ src/CursesUtils.cpp
@@ -7,4 +7,29 @@
 
 #include "CursesUtils.h"
 
+#include <ncurses.h>
 
+void InitCurses(bool hasLineBuffering, bool hasEcho, bool hasKeypad, bool isDynamic, int cursor) {
+	// Initialize curses screen
+	initscr();
+
+	// Remove line buffering
+	if (!hasLineBuffering)	raw();
+
+	// Remove characters display
+	if (!hasEcho)	noecho();
+
+	// Enable/Disable special keys
+	keypad(stdscr, hasKeypad);
+
+	// Enable/Disable the wait time normally included when waiting for user's input
+	nodelay(stdscr, isDynamic);
+
+	// Set the cursor to visible or invisible
+	curs_set(cursor);
+}
+
+void ShutdownCurses() {
+	// Deallocate curses screen/window
+	endwin();
+}
--- src/CursesUtils.h
+++ src/CursesUtils.h
@@ -8,8 +8,20 @@
 #ifndef CURSESUTILS_H_
 #define CURSESUTILS_H_
 
-
-
+/*
+ * Initializes curses library.
+ * hasLineBuffering: If set to true, it will be the default terminal mode.
+ * If set to false, every character will be passed to the program before hitting the newline.
+ * hasEcho: If true, characters will be displayed as they're typed.
+ * If false, characters typed won't be visible.
+ * hasKeypad: True to enable all special key function e.g. keypad.
+ * False will disable them.
+ * isDynamic: If true, curses won't wait for the user to type characters before executing other commands.
+ * If false, it will be the opposite.
+ * cursor: 0 for invisible, 1 for normal visibility, 2 for very visible.
+ */
+void InitCurses(bool hasLineBuffering = false, bool hasEcho = false, bool hasKeypad = true, bool isDynamic = true, int cursor = 0);
+void ShutdownCurses();
 
 
 #endif /* CURSESUTILS_H_ */
