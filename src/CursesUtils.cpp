f0bd90543754a503350418b6bad0ab8ae504d2c4
Daniel Grieco <danny26.business@gmail.com>
2020-03-26 02:12:02 +0100

    Added some functions to get info on the screen as well as functions to output to the screen and move the cursor. I also encapsulated everything into a namespace

--- src/CursesUtils.cpp
+++ src/CursesUtils.cpp
@@ -9,27 +9,95 @@
 
 #include <ncurses.h>
 
-void InitCurses(bool hasLineBuffering, bool hasEcho, bool hasKeypad, bool isDynamic, int cursor) {
-	// Initialize curses screen
-	initscr();
+namespace CursesUtils {
 
-	// Remove line buffering
-	if (!hasLineBuffering)	raw();
+	void InitCurses(bool hasLineBuffering, bool hasEcho, bool hasKeypad, bool isDynamic, int cursor) {
+		// Initialize curses screen
+		initscr();
 
-	// Remove characters display
-	if (!hasEcho)	noecho();
+		// Remove line buffering
+		if (!hasLineBuffering)	raw();
 
-	// Enable/Disable special keys
-	keypad(stdscr, hasKeypad);
+		// Remove characters display
+		if (!hasEcho)	noecho();
 
-	// Enable/Disable the wait time normally included when waiting for user's input
-	nodelay(stdscr, isDynamic);
+		// Enable/Disable special keys
+		keypad(stdscr, hasKeypad);
 
-	// Set the cursor to visible or invisible
-	curs_set(cursor);
-}
+		// Enable/Disable the wait time normally included when waiting for user's input
+		nodelay(stdscr, isDynamic);
 
-void ShutdownCurses() {
-	// Deallocate curses screen/window
-	endwin();
+		// Set the cursor to visible or invisible
+		curs_set(cursor);
+	}
+
+
+	void ShutdownCurses() {
+		// Deallocate curses screen/window
+		endwin();
+	}
+
+
+	void RefreshScreen() {
+		refresh();
+	}
+
+
+	int GetRows() {
+		return LINES;
+	}
+
+
+	int GetColumns() {
+		return COLS;
+	}
+
+
+	void GetWindowSize(int& maxX, int& maxY) {
+		// Store the max x and y position of the given window.
+		getmaxyx(stdscr, maxY, maxX);
+	}
+
+
+	void MoveCursorAtPosition(const int x, const int y) {
+		// Set the position of the cursor to the given position.
+		move(y, x);
+	}
+
+
+	void PrintCharAtPosition(const char character, const int x, const int y) {
+		// Don't move the cursor if any of the coordinates aren't set.
+		if (x == -1 || y == -1) {
+			addch(character);
+			return;
+		}
+
+		// Move the cursor at the given position and print the character.
+		mvaddch(character, y, x);
+	}
+
+
+	void PrintStringAtPosition(const char* cString, const int x, const int y) {
+		// Don't move the cursor if any of the coordinates aren't set.
+		if (x == -1 || y == -1) {
+			addstr(cString);
+			return;
+		}
+
+		// Move the cursor at the given position and print the string.
+		mvaddstr(y, x, cString);
+	}
+
+
+	void PrintFormattedAtPosition(const int x, const int y, const char* cString) {
+		// Move the cursor at the given position and print the formatted output.
+		mvprintw(y, x, cString);
+	}
+
+
+	void PrintFormatted(const char* cString) {
+		// Print the formatted output.
+		printw(cString);
+	}
+
 }
--- src/CursesUtils.h
+++ src/CursesUtils.h
@@ -8,20 +8,88 @@
 #ifndef CURSESUTILS_H_
 #define CURSESUTILS_H_
 
-/*
- * Initializes curses library.
- * hasLineBuffering: If set to true, it will be the default terminal mode.
- * If set to false, every character will be passed to the program before hitting the newline.
- * hasEcho: If true, characters will be displayed as they're typed.
- * If false, characters typed won't be visible.
- * hasKeypad: True to enable all special key function e.g. keypad.
- * False will disable them.
- * isDynamic: If true, curses won't wait for the user to type characters before executing other commands.
- * If false, it will be the opposite.
- * cursor: 0 for invisible, 1 for normal visibility, 2 for very visible.
- */
-void InitCurses(bool hasLineBuffering = false, bool hasEcho = false, bool hasKeypad = true, bool isDynamic = true, int cursor = 0);
-void ShutdownCurses();
+namespace CursesUtils {
 
+	/*
+	 * Initializes curses library.
+	 * hasLineBuffering: If set to true, it will be the default terminal mode.
+	 * If set to false, every character will be passed to the program before hitting the newline.
+	 * hasEcho: If true, characters will be displayed as they're typed.
+	 * If false, characters typed won't be visible.
+	 * hasKeypad: True to enable all special key function e.g. keypad.
+	 * False will disable them.
+	 * isDynamic: If true, curses won't wait for the user to type characters before executing other commands.
+	 * If false, it will be the opposite.
+	 * cursor: 0 for invisible, 1 for normal visibility, 2 for very visible.
+	 */
+	void InitCurses(bool hasLineBuffering = false, bool hasEcho = false, bool hasKeypad = true, bool isDynamic = true, int cursor = 0);
+
+	/*
+	 * Shuts down the curses library.
+	 */
+	void ShutdownCurses();
+
+	/*
+	 * Refreshes the screen, so everything can be displayed properly.
+	 */
+	void RefreshScreen();
+
+	/*
+	 * Returns the number of rows on the screen.
+	 */
+	int GetRows();
+
+	/*
+	 * Returns the number of columns on the screen.
+	 */
+	int GetColumns();
+
+	/*
+	 * Gets the max numbers of rows and cols on the screen.
+	 * maxX: # of columns.
+	 * maxY: # of rows.
+	 */
+	void GetWindowSize(int& maxX, int& maxY);
+
+	/*
+	 * Moves the cursor at the given position on the screen.
+	 * x: Horizontal position on the screen.
+	 * y: Vertical position on the screen.
+	 */
+	void MoveCursorAtPosition(const int x, const int y);
+
+	/*
+	 * Moves the cursor to the given position and prints a character (attributes can be OR'd with the character) at that position.
+	 * character: The character to print.
+	 * x: Horizontal position on the screen.
+	 * y: Vertical position on the screen.
+	 * If those are left blank then it won't move the character at a position.
+	 */
+	void PrintCharAtPosition(const char character, const int x = -1, const int y = -1);
+
+	/*
+	 * Moves the cursor to the given position and prints the given string at that position.
+	 * cString: The string to print.
+	 * x: Horizontal position on the screen.
+	 * y: Vertical position on the screen.
+	 * If those are left blank then it won't move the character at a position.
+	 */
+	void PrintStringAtPosition(const char* cString, const int x = -1, const int y = -1);
+
+	/*
+	 * Moves the cursor to the given position and prints a formatted output at that position.
+	 * cString: The formatted output (Make sure to use this the same way you use printf).
+	 * x: Horizontal position on the screen.
+	 * y: Vertical position on the screen.
+	 */
+	void PrintFormattedAtPosition(const int x, const int y, const char* cString);
+
+	/*
+	 * Prints a formatted output at the cursor's current position.
+	 * cString: The formatted output (Make sure to use this the same way you use printf).
+	 */
+	void PrintFormatted(const char* cString);
+
+}
 
 #endif /* CURSESUTILS_H_ */
