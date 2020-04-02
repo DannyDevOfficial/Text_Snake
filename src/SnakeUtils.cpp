079620fe83c4c3365258880719ec90f25e761b07
Daniel Grieco <danny26.business@gmail.com>
2020-04-02 19:48:19 +0200

    Made header and source files for all the utilities the game will use

---
+++ src/SnakeUtils.cpp
@@ -0,0 +1,10 @@
+/*
+ * SnakeUtils.cpp
+ *
+ *  Created on: Apr 2, 2020
+ *      Author: danielgrieco
+ */
+
+#include "SnakeUtils.h"
+
+
---
+++ src/SnakeUtils.h
@@ -0,0 +1,15 @@
+/*
+ * SnakeUtils.h
+ *
+ *  Created on: Apr 2, 2020
+ *      Author: danielgrieco
+ */
+
+#ifndef SNAKEUTILS_H_
+#define SNAKEUTILS_H_
+
+
+
+
+
+#endif /* SNAKEUTILS_H_ */
